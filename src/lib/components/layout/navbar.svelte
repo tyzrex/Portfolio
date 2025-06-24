4a3acb4b4485811ebd09962986040e3485c3a0e2
tyzrex <sulavbaral58@gmail.com>
2025-06-24 10:20:25 +0545

    basic setup and importing components to files

---
+++ src/lib/components/layout/navbar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+</script>
+
+<header>
+	<div class="border-b border-b-gray-200 bg-white py-8">
+		<div class="container flex justify-between">
+			<div class="averta-bold text-4xl md:text-[45px]">
+				{`<sb`}
+				<span class="text-accent-red">.</span>
+				{`/>`}
+			</div>
+			<nav>
+				<ul class="flex space-x-4">
+					<li><a href="/" class="text-gray-700 hover:text-gray-900">Home</a></li>
+					<li><a href="/about" class="text-gray-700 hover:text-gray-900">About</a></li>
+					<li><a href="/contact" class="text-gray-700 hover:text-gray-900">Contact</a></li>
+					<li><a href="/blog" class="text-gray-700 hover:text-gray-900">Blog</a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+</header>
+
+<style>
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Navbar from '$lib/components/layout/navbar.svelte';
+</script>
+
+<div>
+	<Navbar />
+	<main>
+		<h1>Welcome to the SvelteKit App</h1>
+		<p>This is a simple example of a SvelteKit application.</p>
+	</main>
+</div>
+
+<style>
+</style>
